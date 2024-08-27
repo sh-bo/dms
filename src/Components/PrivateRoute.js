25161b99e6b2a3f4a06a2fb1c100558e3825c9b6
INTEK <arvindrajcs@gmail.com>
2024-08-27 13:50:02 +0530

    ..

--- src/Components/PrivateRoute.js
+++ src/Components/PrivateRoute.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
 
-// Mock function to check if user is authenticated
-const isAuthenticated = () => {
-  // Replace this with your actual authentication logic
-  return localStorage.getItem('authToken') !== null;
-};
-
 const PrivateRoute = ({ children }) => {
-  return isAuthenticated() ? children : <Navigate to="/auth" />;
+    const token = localStorage.getItem('tokenKey');
+
+    return token ? children : <Navigate to="/auth" />;
 };
 
 export default PrivateRoute;
